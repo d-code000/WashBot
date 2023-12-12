08e0c5f6ba08bc510840c53df74d70ed22069815
d-code000 <code.threezeros@gmail.com>
2023-12-12 19:32:11 +0300

    add parser module

---
+++ config.py
@@ -0,0 +1,1 @@
+SITE_URL = "https://cabinet.unimetriq.com/client/d95fe2e686097d1d7d545d138f7a452a"
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import asyncio
+import yaml
+import logging.config
+
+import webparser
 
 
 async def main():
-    pass
+    logging_config = yaml.safe_load(open("logging.yaml", 'rt').read())
+    logging.config.dictConfig(logging_config)
+    logger = logging.getLogger(__name__)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
